a51c60860c355b7ea93a244580f4dc92ef38f6b1
ChSorayuth <adamsoryut@gmail.com>
2020-01-02 13:21:05 +0700

    backup

---
+++ FOL4AutoClick/Form1.vb
@@ -0,0 +1,72 @@
+﻿Public Class Form1
+    Private Sub BackgroundWorker1_DoWork(sender As Object, e As System.ComponentModel.DoWorkEventArgs) Handles BackgroundWorker1.DoWork
+        While Not BackgroundWorker1.CancellationPending
+
+            If GetColor.GETCOLOR(575, 361) = "0x10348F" Then
+                AutoIt.AutoItX.ControlClick("FIFA Online4", "", "", "left", 1, 300, 402)
+                AutoIt.AutoItX.Sleep(500)
+            End If 'Click Manager in Home
+
+            If GetColor.GETCOLOR(1006, 666) = "0xA4EA0D" Then
+                'AutoIt.AutoItX.MouseMove(1006, 666)
+                'AutoIt.AutoItX.MouseClick("left",)
+            End If 'Click Manager in Manager Menu
+
+            If GetColor.GETCOLOR(981, 482) = "0x446C1E" Then
+                AutoIt.AutoItX.MouseMove(1068, 718)
+                AutoIt.AutoItX.MouseClick("left",)
+                AutoIt.AutoItX.Sleep(500)
+            End If 'Click Manager in Manager Menu2
+
+            'If GetColor.GETCOLOR(308, 725) = "0xA4EA0D" Then
+            'Else
+            '    AutoIt.AutoItX.ControlSend("FIFA Online4", "", "", "s")
+            '    AutoIt.AutoItX.Sleep(500)
+            'End If 'Skip Replay
+
+            'If GetColor.GETCOLOR(294, 650) = "0x151532" Then
+            '    AutoIt.AutoItX.ControlSend("FIFA Online4", "", "", "{SPACE}")
+            '    AutoIt.AutoItX.Sleep(500)
+            'End If 'Spacebar END
+
+            If GetColor.GETCOLOR(1066, 718) = "0xA3E90C" Then
+                AutoIt.AutoItX.MouseMove(1066, 718)
+                AutoIt.AutoItX.MouseClick("left",)
+                AutoIt.AutoItX.Sleep(500)
+            End If 'manager rank , level up , etc
+
+            If GetColor.GETCOLOR(1075, 719) = "0xA3E90C" Then
+                AutoIt.AutoItX.MouseMove(1075, 719)
+                AutoIt.AutoItX.MouseClick("left",)
+                AutoIt.AutoItX.Sleep(500)
+            End If 'END
+        End While
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Try
+            BackgroundWorker1.RunWorkerAsync()
+        Catch ex As Exception
+
+        End Try
+        Button1.Enabled = False
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        BackgroundWorker1.CancelAsync()
+        Button2.Enabled = False
+    End Sub
+
+    Private Sub Form1_FormClosing(sender As Object, e As FormClosingEventArgs) Handles Me.FormClosing
+        Application.Exit()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        MsgBox("1280x720 (16:9)")
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        Application.Restart()
+        Application.Exit()
+    End Sub
+End Class
---
+++ FOL4AutoClick/GetColor.vb
@@ -0,0 +1,39 @@
+﻿Imports System.Runtime.InteropServices
+Imports AutoIt.AutoItX
+Module GetColor
+    '------------ Function ที่ใช้ ในการดึงค่าสี จาก เบื้องหลัง Begin ----------------//
+    <DllImport("user32.dll", SetLastError:=True)>
+    Public Function GetWindowDC(window As IntPtr) As IntPtr
+    End Function
+
+    <DllImport("gdi32.dll", SetLastError:=True)>
+    Public Function GetPixel(dc As IntPtr, x As Integer, y As Integer) As UInteger
+    End Function
+
+    <DllImport("user32.dll", SetLastError:=True)>
+    Public Function ReleaseDC(window As IntPtr, dc As IntPtr) As Integer
+    End Function
+
+    Public Function GetColorAt(hwnd As IntPtr, x As Integer, y As Integer) As Color
+        Try
+            Dim dc As IntPtr = GetWindowDC(hwnd)
+            Dim a As Integer = CInt(GetPixel(dc, x, y))
+            ReleaseDC(hwnd, dc)
+            Return Color.FromArgb(255, (a >> 0) And &HFF, (a >> 8) And &HFF, (a >> 16) And &HFF)
+        Catch ex As Exception
+
+        End Try
+
+    End Function
+
+    Public Function HexConverter(c As System.Drawing.Color) As [String]
+        Return [String].Format("0x{0}", c.R.ToString("X2") + c.G.ToString("X2") + c.B.ToString("X2"))
+    End Function
+
+    '------------ Function ที่ใช้ ในการดึงค่าสี จาก เบื้องหลัง Begin ----------------//
+    Public Function GETCOLOR(x As Integer, y As Integer)
+        Dim appHandle As IntPtr = AutoIt.AutoItX.WinGetHandle("FIFA Online4", "") ''ชื่อเกมส์
+        Return HexConverter(GetColorAt(appHandle, Integer.Parse(x), Integer.Parse(y)))
+    End Function
+
+End Module
